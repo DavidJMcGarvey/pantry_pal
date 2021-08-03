1eaacc94166a7013df01d03e27be2f1818f7510f
David McGarvey <david@citymac.com>
2021-08-03 13:07:33 -0700

    add CSS to addItem

--- js/pantryPal.js
+++ js/pantryPal.js
@@ -2,7 +2,7 @@ const newItem = document.querySelector('.addItemInput');
 // console.log(newItem.value);
 function addItem() {
     let item = `<li class=\"quote\">${newItem.value}</li>`;
-    document.getElementById('inventory-list').innerHTML += newItem.value;
+    document.getElementById('inventory-list').innerHTML += item;
 }
 
 document.getElementById('addItemButton').addEventListener("click", addItem, false);
